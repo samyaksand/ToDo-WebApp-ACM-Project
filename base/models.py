d8a4170615ac84dfef8058c9a10eab4804deab3d
sree dev <sreedev0115@gmail.com>
2021-07-20 22:48:00 +0300

    Added Date, Priority

--- base/models.py
+++ base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime
 # Create your models here.
 
 
@@ -10,6 +12,9 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=False,auto_now=False,blank=True)
+    priority = models.IntegerField(blank=True,default=1,null=True,validators=[MaxValueValidator(10), MinValueValidator(0)])
+
 
     def __str__(self):
         return self.title
